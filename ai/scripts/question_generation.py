339638752491d0807a23be242b9eec0967d040f8
Dominik Bilski <dominik.bilski24@gmail.com>
2023-03-31 17:43:08 +0200

    Make gpt answer in JSONs.

--- ai/scripts/question_generation.py
+++ ai/scripts/question_generation.py
@@ -1,31 +1,26 @@
 import sys
-import openai
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import (
     ChatPromptTemplate,
-    PromptTemplate,
     SystemMessagePromptTemplate,
-    AIMessagePromptTemplate,
     HumanMessagePromptTemplate,
-)
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
 )
 
 
 def main(subject):
     chat = ChatOpenAI(temperature=0)
-    template = f'You are helping to generate dataset of questions and answers for IT recruitement process related to {subject}.'
-    system_prompt_message = SystemMessagePromptTemplate.from_template(template)
+    template = 'You are helping to generate dataset of questions for IT recruitement process related to {subject}.'
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 
-    human_template = "Generate a question in a JSON format."
-    human_prompt_message = HumanMessagePromptTemplate.from_template(
+    human_template = 'Generate a question. Write it out only in JSON format(no other text). The JSON should only have one field: "question"'
+    human_message_prompt = HumanMessagePromptTemplate.from_template(
         human_template)
+
     chat_prompt = ChatPromptTemplate.from_messages(
-        [system_prompt_message, human_prompt_message])
-    print(chat(chat_prompt.format_prompt().to_messages()))
+        [system_message_prompt, human_message_prompt])
+
+    formatted_prompt = chat_prompt.format_prompt(subject=subject).to_messages()
+    print(chat(formatted_prompt).content)
 
 
 if __name__ == "__main__":
