35f7bd798db278e3104d240dbf765e49bdf00c01
Dominik Bilski <dominik.bilski24@gmail.com>
2023-03-31 17:27:58 +0200

    Add templates for prompts and script argument for topic. Return answer from gpt in JSON format.

--- ai/scripts/question_generation.py
+++ ai/scripts/question_generation.py
@@ -1,6 +1,36 @@
+import sys
 import openai
-from langchain.llms import OpenAIChat
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
 
-llm = OpenAIChat()
-text = "Write a question for javascript interview?"
-print(llm(text))
+
+def main(subject):
+    chat = ChatOpenAI(temperature=0)
+    template = f'You are helping to generate dataset of questions and answers for IT recruitement process related to {subject}.'
+    system_prompt_message = SystemMessagePromptTemplate.from_template(template)
+
+    human_template = "Generate a question in a JSON format."
+    human_prompt_message = HumanMessagePromptTemplate.from_template(
+        human_template)
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_prompt_message, human_prompt_message])
+    print(chat(chat_prompt.format_prompt().to_messages()))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        subject = sys.argv[1]
+        main(subject)
+    else:
+        print("Please provide a subject as a command line argument.")
