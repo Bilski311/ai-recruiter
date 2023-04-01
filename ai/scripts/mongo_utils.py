09170030e975a96d1bde04e4aa4a9fffbfcead1d
Dominik Bilski <dominik.bilski24@gmail.com>
2023-04-01 18:59:26 +0200

    Modify mongo config.

--- ai/scripts/mongo_utils.py
+++ ai/scripts/mongo_utils.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
 from config import MONGO_URI, MONGO_DB, MONGO_COLLECTION
 
+client = MongoClient(MONGO_URI)
+db = client[MONGO_DB]
+
+
 def save_to_mongo(result, collection):
-    client = MongoClient(MONGO_URI)
-    db = client[MONGO_DB]
     collection = db[collection]
     collection.insert_many(result)
     client.close()
