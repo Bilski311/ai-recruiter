c1f9ce2a44b93cfb0317f1631f51009f51551b99
Dominik Bilski <dominik.bilski24@gmail.com>
2023-04-01 17:54:11 +0200

    Naming improvement for generation scripts.

--- ai/scripts/config.py
+++ ai/scripts/config.py
@@ -1,14 +1,14 @@
 CHAT_TEMPERATURE = 0
 output_types = {
-    'topic': {
+    'TOPIC': {
         'system_template': '',
         'human_template': ''
     },
-    'subtopic': {
+    'SUBTOPIC': {
         'system_template': 'You are helping to generate a list of subtopics one should know for the given topic: {subject}.',
         'human_template': 'Generate a list of 50 subtopics in JSON format. Write it out only in JSON format(no other text). The JSON should have the format [{{"subtopic": "Subtopic 1"}}, {{"subtopic": "Subtopic 2"}}].'
     },
-    'question': {
+    'QUESTION': {
         'system_template': 'You are helping to generate dataset of questions for IT recruitement process related to {subject}.',
         'human_template': 'Generate a question. Write it out only in JSON format(no other text). The JSON should only have one field: "question"'
     }
--- ai/scripts/generate.py
+++ ai/scripts/generate.py
@@ -4,21 +4,24 @@ from chat_utils import generate_chat_output
 from config import output_types
 
 
-def generate(type, subject):
-    output_type = output_types[type]
+def generate(output_type, subject_type):
+    output_type_config = output_types[output_type]
+    system_template = output_type_config['system_template']
+    human_template = output_type_config['human_template']
+
     output = generate_chat_output(
-        subject, output_type['system_template'], output_type['human_template'])
+        subject_type, system_template, human_template)
     print(output)
 
 
 if __name__ == "__main__":
     if len(sys.argv) <= 2:
         print("Proper usage: python3 generate.py <output_type> <subject_type>")
-    elif sys.argv[1] not in output_types.keys():
+    elif sys.argv[1].upper() not in output_types.keys():
         print("Available types: ")
         for key in output_types.keys():
             print(key)
     else:
-        type = sys.argv[1]
-        subject = sys.argv[2]
-        generate(type, subject)
+        output_type = sys.argv[1].upper()
+        subject_type = sys.argv[2]
+        generate(output_type, subject_type)
