9d4ee2db55f55e1a4f0973c4c66e97008359889b
Dominik Bilski <dominik.bilski24@gmail.com>
2023-04-02 10:04:04 +0200

    Add working connection to mongo.

--- ai/scripts/config.py
+++ ai/scripts/config.py
@@ -1,5 +1,5 @@
 CHAT_TEMPERATURE = 0
-MONGO_URI = "mongodb://localhost:27017"
+MONGO_URI = "<connection_string>"
 MONGO_DB = "ai_recruiter"
 MONGO_COLLECTION = "generated_content"
 SAVE_TO_MONGO = False
@@ -7,7 +7,7 @@ SAVE_TO_MONGO = False
 output_types = {
     'TOPIC': {
         'system_template': 'You are helping to generate a list of technical topics. They should group questions related to the recruitement process for the position of {subject}.',
-        'human_template': 'Generate a list of 10 topics in JSON format. Write it out only in JSON format(no other text). The JSON should have the format [{{"name": "Topic 1"}}, {{"name": "Topic 2"}}].',
+        'human_template': 'Generate a list of 10 topics in JSON format. Write it out only in JSON format(no other text). The JSON should have the format [{{"name": "Topic 1"}}, {{"name": "Topic 2"}}]. Exclude any characters that are not part of the JSON(even the ones you use to properly display it in chat window)',
         'collection': 'topics'
     },
     'SUBTOPIC': {
--- ai/scripts/mongo_utils.py
+++ ai/scripts/mongo_utils.py
@@ -1,7 +1,12 @@
+import ssl
 from pymongo import MongoClient
-from config import MONGO_URI, MONGO_DB, MONGO_COLLECTION
+from config import MONGO_URI, MONGO_DB
+import certifi
 
-client = MongoClient(MONGO_URI)
+
+client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+db = client.test
+
 db = client[MONGO_DB]
 
 
