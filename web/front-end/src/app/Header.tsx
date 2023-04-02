5773baec27321c4cc7b07da6f32147d94465466e
Maks Padalak <maksymilian.padalak@gmail.com>
2023-04-02 21:38:34 +0200

    Animate a header

--- web/front-end/src/app/Header.tsx
+++ web/front-end/src/app/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="p-5 flex items-center justify-center bg-gradient-to-r from-indigo-600 via-pink-500 to-purple-500">
+    <header className="p-5 flex items-center justify-center bg-gradient-to-r from-indigo-600 via-pink-500 to-purple-700 animate-gradient">
       <p className="font-roboto text-white font-semibold text-3xl uppercase tracking-wider animate-fade-in">
         Futuristic Job Market
       </p>
